e1a3bce1d76a3dc512f0710ae7a820cf6e6550f3
shake <hiroki.yamako@ezweb.ne.jp>
2021-02-18 23:47:22 +0900

    createページの追加とtaskの表示可能個数の修正

--- all_list/forms.py
+++ all_list/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ToDoForm(forms.Form):
-    end = forms.BooleanField()
-    task = forms.CharField(max_length=100)
-    limit = forms.DateField()
-    memo = forms.CharField(max_length=500)
+    end = forms.BooleanField(label='完了', required=False)
+    task = forms.CharField(label='title', max_length=100)
+    limit = forms.DateField(label='期限')
+    memo = forms.CharField(label='詳細', max_length=500, required=False)
--- all_list/urls.py
+++ all_list/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_cal, name='show_cal'),
+    path('cal', views.show_cal, name='show_cal'),
+    path('create_todo', views.create_todo, name='create_todo')
 ]

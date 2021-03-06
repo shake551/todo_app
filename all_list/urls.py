f8b25b0f294a57155a4e344fc819e643137718eb
shake <hiroki.yamako@ezweb.ne.jp>
2021-03-06 17:15:12 +0900

    [add]ページネーション追加 ボタンを押すことで前の月または次の月に移動できるようにした

--- all_list/urls.py
+++ all_list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cal', views.show_cal, name='show_cal'),
+    path('cal/<int:year>/<int:month>', views.show_cal, name='show_cal'),
     path('create', views.create, name='create'),
     path('edit/<int:num>/', views.edit, name='edit'),
     path('delete/<int:num>/', views.delete, name='delete'),
